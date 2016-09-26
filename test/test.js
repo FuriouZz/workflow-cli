f0bba38fe33418441a23c829b99e5a760298998b
Christophe Massolin <christophe.massolin@makemepulse.com>
2016-09-26 18:58:38 +0200

    [Task] Add some tests on task

--- test/test.js
+++ test/test.js
@@ -16,14 +16,64 @@ describe('API', function() {
     assert(fail)
   })
 
+})
+
+describe('TASK', function() {
+
   it('task without action', function() {
-    wk.run('no_action').then(function(value) {
-      assert(value)
-    })
+    wk.run('no_action')
   })
 
   it('task with action setted later', function() {
     wk.run('action_later')
   })
+
+  describe('SERIE', function() {
+
+
+    it('each task must have the right values', function( done ) {
+
+      wk.run('serie0').then(function(value) {
+        assert.equal(wk.Tasks['task_sync'].value, 'task_sync:complete')
+        assert.equal(wk.Tasks['task_async'].value, 'task_async:complete')
+        assert.equal(value, 'serie0:complete')
+        done()
+      }).catch(function(err) {
+        done(err)
+      })
+
+    })
+
+    it('tasks must be executed in the right order', function( done ) {
+
+      wk.Tasks['serie0'].reenable()
+      wk.Tasks['serie0'].invoke()
+
+      // Executed in the right order
+      let order = -1
+
+      wk.Tasks['task_sync'].promise.then(function() {
+        order++
+        assert.equal(order, 0)
+      })
+
+      wk.Tasks['task_async'].promise.then(function() {
+        order++
+        assert.equal(order, 1)
+      })
+
+      wk.Tasks['serie0'].promise.then(function() {
+        order++
+        assert.equal(order, 2)
+
+        done()
+      })
+
+    })
+
+
+  })
+
+
 
 })
