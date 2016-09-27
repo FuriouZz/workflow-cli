24313a05965f07c67aea70f88f62773c9c3dd675
Christophe Massolin <christophe.massolin@makemepulse.com>
2016-09-27 12:14:39 +0200

    Set API methods to global only from cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-require('./../lib/workflow.js')
+const wk  = require('./../lib/workflow.js')
+global.wk = wk
+
+const Command = require('./../lib/command')
+const API     = require('./../lib/api')
+
+// Check no global property overrided
+const API_KEYS = Object.keys(API)
+
+for (const i in API_KEYS) {
+  if (global.hasOwnProperty(API_KEYS[i])) {
+   throw new Error(`The api method ${API_KEYS[i]} override a global property`)
+  }
+}
+
+// Export API to global
+Object.assign(global, API)
+
+// Load Wkfile
+wk.load(wk.PARAMS.file)
+
+// Execute command
+Command()
