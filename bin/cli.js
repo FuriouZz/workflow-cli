4e218f8bcd7c2230a01e29ff854b9a0e8c2a882c
Christophe Massolin <christophe.massolin@gmail.com>
2016-10-10 01:57:46 +0200

    Enhance workflow in node context

--- bin/cli.js
+++ bin/cli.js
@@ -8,18 +8,6 @@ const ARGParser = require('./../lib/arg-parser')
 wk.PARAMS = ARGParser.parse(process.argv.slice(2))
 wk.ARGV   = wk.PARAMS.__
 
-// Check no global property overrided
-const API_KEYS = Object.keys(wk.api)
-
-for (const i in API_KEYS) {
-  if (global.hasOwnProperty(API_KEYS[i])) {
-   throw new Error(`The api method ${API_KEYS[i]} override a global property`)
-  }
-}
-
-// Export API to global
-Object.assign(global, wk.api)
-
 // Load Wkfile
 wk.load(wk.PARAMS.file)
 
