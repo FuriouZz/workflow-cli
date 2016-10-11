37c6e8d1edbd59940b0b5bd02d0740b1140bd26d
Christophe Massolin <christophe.massolin@gmail.com>
2016-10-11 03:26:17 +0200

    Completely rework ProcessManager

---
+++ test/test_exec.js
@@ -0,0 +1,12 @@
+setTimeout(function() {
+  console.log('Hello World')
+}, 1000)
+setTimeout(function() {
+  console.log('Surprise')
+}, 2000)
+setTimeout(function() {
+  console.log('Motherfucka')
+}, 3000)
+// setTimeout(function() {
+//   throw 'yolo'
+// }, 4000)
