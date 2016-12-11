796478557eb75f60802671d280cd001cb3d1ae65
Christophe Massolin <christophe.massolin@gmail.com>
2016-12-11 01:24:23 +0100

    [ARGParser] Split wk parameters and task parameters

--- bin/cli.js
+++ bin/cli.js
@@ -4,6 +4,11 @@ const wk        = require('./../lib/workflow.js')
 const Command   = require('./../lib/command')
 const ARGParser = require('./../lib/arg-parser')
 
+// const parameters = ARGParser.splitParameters( process.argv.slice(2) )
+// const WKPARAMS = ARGParser.parse(parameters.wkParameters)
+// const TKPARAMS = ARGParser._softParse(parameters.taskParameters)
+// console.log(WKPARAMS, TKPARAMS)
+
 // Parse arguments
 wk.PARAMS = ARGParser.parse(process.argv.slice(2))
 wk.ARGV   = wk.PARAMS.__
