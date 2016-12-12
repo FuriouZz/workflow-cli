7130ebd1d2b19f8d111ba8bcfadf9e0bcd0dab81
Christophe Massolin <christophe.massolin@gmail.com>
2016-12-12 01:17:23 +0100

    Break task parameter system

--- bin/cli.js
+++ bin/cli.js
@@ -2,16 +2,20 @@
 
 const wk        = require('./../lib/workflow.js')
 const Command   = require('./../lib/command')
-const ARGParser = require('./../lib/arg-parser')
 
-// const parameters = ARGParser.splitParameters( process.argv.slice(2) )
-// const WKPARAMS = ARGParser.parse(parameters.wkParameters)
-// const TKPARAMS = ARGParser._softParse(parameters.taskParameters)
-// console.log(WKPARAMS, TKPARAMS)
+const ARGParser  = require('./../lib/arg-parser')
+const ARGConfig = require('./../lib/config/parameters')
+
+const p = ARGParser.splitParameters( process.argv.slice(2), ARGConfig )
 
 // Parse arguments
-wk.PARAMS = ARGParser.parse(process.argv.slice(2))
+wk.PARAMS = ARGParser.parse(p.wkParameters, ARGConfig)
 wk.ARGV   = wk.PARAMS.__
+
+if (p.taskParameters) {
+  wk.ARGV._.push( p.taskParameters[0] )
+  wk.ARGV[p.taskParameters[0]+'_argv'] = p.taskParameters.slice(1)
+}
 
 // Load Wkfile
 wk.load(wk.PARAMS.file)
